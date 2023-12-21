9f7910737b453270522a7fd7cab29f4976c0ffdb
rramka <ramazikapanadze@gmail.com>
2023-12-22 02:27:01 +0400

    incremental loding2

---
+++ myFramework/utils/getDF.py
@@ -0,0 +1,14 @@
+import myFramework.source.posgresql.connect as conn
+import pandas as pd
+
+
+
+class getDF:
+
+    def __init__(self) -> None:
+        pass
+
+
+    def getDF():
+        pass
+   
