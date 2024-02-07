1cf94a547965faab52811be1823970cefd50aca9
shotatsuleiskiri <tsuleiskirishota@gmail.com>
2024-02-08 00:45:08 +0400

    dv-mde swori

--- main.py
+++ main.py
@@ -10,26 +10,38 @@ from stagingtodv.nodatemanaged.toDV import ToDV
 # ---------------------------DV----------------
 
 
-# Full
+# Full - SCDType1
 # create ReadYaml object
 
-# testread = ReadYaml("/Users/ramazkapanadze/DEProject/DEProject/conf/toDV/dvdrental/full.yaml", 'dvdrental.store')
+# testread = ReadYaml("/Users/mariammakharadze/PycharmProjects/DEProject/conf/toDV/dvdrental/SCDType1.yaml", 'dvdrental.language')
 # test = ToDV(testread.path, testread.key)
 # sourceDF = utils.getDF(test.getSourceDBName(), test.getTSourceTableName(),test.getSourceSchema())
 # ganaretedDF = utils.generateSurogateKey(sourceDF,test.getCode(), test.getNaturalKey() )
 # utils.fillPosgres(ganaretedDF,f'{test.getDestDBName()}',f'{test.getDestSchema()}',test.getDestTbaleName())
-
-
+#
 
 # incremental
 # create ReadYaml object
 
-testread = ReadYaml("/Users/ramazkapanadze/DEProject/DEProject/conf/toDV/dvdrental/incremental.yaml", 'dvdrental.rental')
-test = ToDV(testread.path, testread.key)
-sourceDF = utils.getDF(test.getSourceDBName(), test.getTSourceTableName(),test.getSourceSchema(),test.getfilterColumn(), "2006-02-14", "2006-02-15")
-ganaretedDF = utils.generateSurogateKey(sourceDF,test.getCode(), test.getNaturalKey() )
-utils.fillPosgres(ganaretedDF,f'{test.getDestDBName()}',f'{test.getDestSchema()}',test.getDestTbaleName())
+# testread = ReadYaml("/Users/mariammakharadze/PycharmProjects/DEProject/conf/toDV/dvdrental/incremental.yaml", 'dvdrental.film_category')
+# test = ToDV(testread.path, testread.key)
+# sourceDF = utils.getDF(test.getSourceDBName(), test.getTSourceTableName(),test.getSourceSchema(),test.getfilterColumn(), "2007-02-14", "2006-04-30").drop("insertion_date",  axis=1)
+# genaretedDF = utils.generateSurogateKey(sourceDF,test.getCode(), test.getNaturalKey() )
+# # print(genaretedDF)
+# utils.fillPosgres(genaretedDF,f'{test.getDestDBName()}',f'{test.getDestSchema()}',test.getDestTbaleName())
+#
 
+# SCDType2
+
+testread = ReadYaml("/Users/mariammakharadze/PycharmProjects/DEProject/conf/toDV/dvdrental/SCDType2.yaml", 'dvdrental.staff')
+test = ToDV(testread.path, testread.key)
+sourceDF = utils.getDF(test.getSourceDBName(), test.getTSourceTableName(),test.getSourceSchema(),test.getfilterColumn(), "2006-02-16")
+targetDF = utils.getDF(test.getDestDBName(), test.getDestTbaleName(),test.getDestSchema())
+# print(targetDF)
+newSourceDF = utils.toSCD2(sourceDF, targetDF)
+ganaretedDF = utils.generateSurogateKey(newSourceDF,test.getCode(), test.getNaturalKey() )
+print(ganaretedDF)
+utils.fillPosgres(ganaretedDF,f'{test.getDestDBName()}',f'{test.getDestSchema()}',test.getDestTbaleName())
 
 
 
@@ -37,24 +49,26 @@ utils.fillPosgres(ganaretedDF,f'{test.getDestDBName()}',f'{test.getDestSchema()}
 # # initial
 # test = toStaging_initial('dvdrental','public')
 # tbname_Df = utils.getTbaleList(test.getSourceDBName,test.getSourceSchema)
-# for tbname in tbname_Df['tablename']:    
+# for tbname in tbname_Df['tablename']:
 #     utils.fillPosgres(utils.getDF( tbname ,test.getSourceSchema),'DBStaging','dvdrental', tbname)
-  
-   
+
+
 # full load
-# testread = ReadYaml("/Users/ramazkapanadze/DEProject/DEProject/conf/tostaging/dvdrental/full.yaml", 'public.category')
+# testread = ReadYaml("/Users/mariammakharadze/PycharmProjects/DEProject/conf/tostaging/dvdrental/full.yaml", 'public.inventory')
 # test = toStaging_full(testread.path, testread.key)
 # sourceDF = utils.getDF(test.getSourceDBName(), test.getTSourceTableName(),test.getSourceSchema())
 # utils.fillPosgres(sourceDF,f'{test.getDestDBName()}',f'{test.getDestSchema()}',test.getDestTbaleName())
 
-# incremental 
-# testread = ReadYaml("/Users/ramazkapanadze/DEProject/DEProject/conf/tostaging/dvdrental/incremental.yaml", 'public.payment')
+
+# incremental
+# testread = ReadYaml("/Users/mariammakharadze/PycharmProjects/DEProject/conf/tostaging/dvdrental/incremental.yaml", 'public.customer')
 # test = toStaging_incremental(testread.path, testread.key)
-# sourceDF = utils.getDF(test.getSourceDBName(), test.getTSourceTableName(),test.getSourceSchema(),test.getfilterColumn(), "2007-02-10", "2007-05-16")
-# utils.fillPosgres(sourceDF,f'{test.getDestDBName()}',f'{test.getDestSchema()}',test.getDestTbaleName())
+# sourceDF = utils.getDF(test.getSourceDBName(), test.getTSourceTableName(),test.getSourceSchema(),test.getfilterColumn(), "2000-02-10", "2024-02-16")
+# newsourceDF = utils.addInsertionDate(sourceDF)
+# # print(newsourceDF)
+# utils.fillPosgres(newsourceDF,f'{test.getDestDBName()}',f'{test.getDestSchema()}',test.getDestTbaleName())
 
 # clean staging
-# cleantestread = ReadYaml("/Users/ramazkapanadze/DEProject/DEProject/conf/tostaging/dvdrental/full.yaml", 'public.category')
+# cleantestread = ReadYaml("/Users/mariammakharadze/PycharmProjects/DEProject/conf/tostaging/dvdrental/full.yaml", 'public.category')
 # t = CleanStaging()
 # t.cleanStaging('DBStaging', 'dvdrental', 'category', 2023, 12, 28)
-
--- myFramework/utils/utils.py
+++ myFramework/utils/utils.py
@@ -1,6 +1,8 @@
 import myFramework.source.posgresql.connect as conn
 import pandas as pd
 from multipledispatch import dispatch
+from datetime import datetime
+from pandas_scd import scd2
 
 
 # import sys
@@ -18,20 +20,33 @@ def posgreExecute(dbName, query):
 
 @dispatch(str, str, str)
 def getDF( source_dbname, tablename, schema):
-    query = f"select T.*,  DATE(CURRENT_TIMESTAMP) insertion_date from {schema}.{tablename} T"
+    query = f"select T.* from {schema}.{tablename} T"
+    return pd.read_sql(query ,conn.getConnection(source_dbname))
+
+@dispatch(str, str, str, str, str)
+def getDF( source_dbname, tablename,schema,filterColumn, dateFrom):
+    query = f"select T.* from {schema}.{tablename} T where {filterColumn} >= '{dateFrom}'"
     return pd.read_sql(query ,conn.getConnection(source_dbname))
 
 @dispatch(str, str, str, str, str,str)
 def getDF( source_dbname, tablename,schema,filterColumn, dateFrom, dateTo):
-    query = f"select T.*, DATE(CURRENT_TIMESTAMP) insertion_date from {schema}.{tablename} T where {filterColumn} >= '{dateFrom}' and {filterColumn} < '{dateTo}' "
+    query = f"select T.* from {schema}.{tablename} T where {filterColumn} >= '{dateFrom}' and {filterColumn} < '{dateTo}' "
     return pd.read_sql(query ,conn.getConnection(source_dbname))
+
+def addInsertionDate(df: pd.DataFrame ):
+      return df.assign(insertion_date = lambda x: datetime.now())
 
 
 def generateSurogateKey(df, code,NaturalKey):
       newdf = pd.DataFrame(df)
       NaturalKey = newdf[NaturalKey]+int(str(1000000) + str(code))
-      return newdf.assign(surogatekey = pd.Series(NaturalKey).values  ) 
+      return newdf.assign(surogatekey = pd.Series(NaturalKey).values)
 
 def fillPosgres( df, dst_dbname, schema, tablename):
         df.to_sql(tablename, conn.getConnection(dst_dbname)
                 , schema=f"{schema}", if_exists='replace', index=False)
+
+
+def toSCD2(srcDF: pd.DataFrame, targetDF: pd.DataFrame):
+      final_df = scd2(srcDF, targetDF)
+      return final_df
